9ede64c67ac86f60223e47dcffc58da67d1c13ec
TahmKillerB <mohamado.ezedino@gmail.com>
2022-05-10 18:53:55 +0100

    Fixed minor issues

--- AdminServer/src/main/java/com/example/demo/AdminServerApplication.java
+++ AdminServer/src/main/java/com/example/demo/AdminServerApplication.java
@@ -8,9 +8,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 @SpringBootApplication
 @EnableAdminServer
 public class AdminServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AdminServerApplication.class, args);
 	}
-
 }
